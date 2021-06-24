346711a7c09f2993f967d2eea1f735c4e1dee294
gluemchen <gluemchen81@googlemail.com>
2021-06-24 17:03:43 +0200

    Added pygame tutorial code and ball gif

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-import math
-import sys
-from os import rename
+import sys, pygame
 
-import requests
+pygame.init()
 
-print(sys.version)
-print(sys.executable)
+size = width, height = 320, 240
+speed = [2, 2]
+black = 0, 0, 0
 
+screen = pygame.display.set_mode(size)
 
-def greet(who_to_greet):
-    test = "Test"
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
+ball = pygame.image.load("intro_ball.gif")
+ballrect = ball.get_rect()
 
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-r = requests.get("https://coreyms.com")
-print(r.status_code)
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
