a8ffa71a21dde0813beeed26c21a2adac92cf69c
gluemchen <gluemchen81@googlemail.com>
2021-07-03 17:00:06 +0200

    Working added RGB via keypress

--- chapter1.py
+++ chapter1.py
@@ -1,12 +1,7 @@
+"""
+Learning some pygame
+"""
 import pygame
-from pygame import *
-from pygame import mixer
-from pygame.locals import *
-
-pygame.init()
-pygame.quit()
-
-screen = display.set_mode((640, 480), RESIZABLE)
 
 # initialized list of colors
 BLACK = (0, 0, 0)
@@ -18,19 +13,26 @@ BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 CYAN = (0, 255, 255)
 MAGENTA = (255, 0, 255)
-background = GRAY
-screen.fill(background)
-pygame.display.update()
 
-running = True
-while running:
+pygame.init()
+screen = pygame.display.set_mode((640, 480), pygame.RESIZABLE)
+
+RUNNING = True
+background = GRAY
+while RUNNING:
     for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            RUNNING = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_r:
-                background == RED
-            elif event.key == pygame.K_r:
-                background == GREEN
-        if event.type == pygame.QUIT:
-            running = False
+                background = RED
+            elif event.key == pygame.K_g:
+                background = GREEN
+            elif event.key == pygame.K_b:
+                background = BLUE
+
+    screen.fill(background)
+    pygame.display.update()
 
 pygame.quit()
