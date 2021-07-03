5838473386d729607a4018b31efef2c791cc3c92
gluemchen <gluemchen81@googlemail.com>
2021-07-04 00:19:15 +0200

    Chapter 1 of pygame

--- chapter1.py
+++ chapter1.py
@@ -1,7 +1,13 @@
 """
 Learning some pygame
 """
+from turtle import speed
 import pygame
+from pygame.locals import *
+
+# resoloution
+size = 1024, 768
+width, height = size
 
 # initialized list of colors
 BLACK = (0, 0, 0)
@@ -14,25 +20,51 @@ YELLOW = (255, 255, 0)
 CYAN = (0, 255, 255)
 MAGENTA = (255, 0, 255)
 
-pygame.init()
-screen = pygame.display.set_mode((640, 480), pygame.RESIZABLE)
+# Keys paired to colors
+key_dict = {
+    K_k: BLACK,
+    K_r: RED,
+    K_g: GREEN,
+    K_b: BLUE,
+    K_y: YELLOW,
+    K_c: CYAN,
+    K_m: MAGENTA,
+    K_w: WHITE,
+}
 
+# initialize all imported pygame modules
+pygame.init()
+
+# initializing a resizable screen
+screen = pygame.display.set_mode((size), RESIZABLE)
 RUNNING = True
 background = GRAY
+
+ball = pygame.image.load("intro_ball.gif")
+rect = ball.get_rect()
+speed = [2, 2]
+
 while RUNNING:
     for event in pygame.event.get():
+        print(key_dict)
         print(event)
-        if event.type == pygame.QUIT:
+        rect = rect.move(speed)
+        if event.type == QUIT:
             RUNNING = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_r:
-                background = RED
-            elif event.key == pygame.K_g:
-                background = GREEN
-            elif event.key == pygame.K_b:
-                background = BLUE
+        if event.type == KEYDOWN:
+            if event.key in key_dict:
+                background = key_dict[event.key]
+                caption = "Background Color = " + str(background)
+                pygame.display.set_caption(caption)
+        if rect.left < 0 or rect.right > width:
+            speed[0] = -speed[0]
+        if rect.top < 0 or rect.bottom > height:
+            speed[1] = -speed[1]
 
     screen.fill(background)
+    pygame.draw.rect(screen, RED, rect, 1)
+    screen.blit(ball, rect)
     pygame.display.update()
 
+# uninitialize all imported pygame modules
 pygame.quit()
