8b1988a4792404111aa900c7765bd79784f58880
Mike Jones <gluemchen81@googlemail.com>
2021-07-16 11:42:31 +0200

    https://www.pythontutorial.net/tkinter/tkinter-command/

--- tkinter_intro.py
+++ tkinter_intro.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 # Initialize TKinter Window with title
 window = tk.Tk()
@@ -20,8 +21,22 @@ center_horizontal = int((screen_width - window_width) / 2)
 center_vertical = int((screen_height - window_height) / 2)
 window.geometry(f"{window_width}x{window_height}+{center_horizontal}+{center_vertical}")
 
+# Makes the windows Size non adjustable
 window.resizable(False, False)
+window.attributes("-topmost", 1)
+window.iconbitmap("./assets/calculator.ico")
 
+# Window Content
+tk.Label(window, text="Classic Label").pack()
+ttk.Label(window, text="Themed Label").pack()
+
+# Define Functions
+def button_pressed():
+    print("You pressed the button!")
+
+
+button = ttk.Button(window, text="Press Me!", command=button_pressed)
+button.pack()
 print(window.title())
 print(window.geometry())
 
